743d02db243c1324e926b5cd3dc0955a74c8741b
BaekInHyeok <bih63879@gmail.com>
2024-08-13 19:47:41 +0900

    Amazon S3 연결, 카드 등록 사진 촬영 업로드 테스트 완료

--- src/pages/CardInfo.jsx
+++ src/pages/CardInfo.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/cardInfo.css";
+import CardDeltePopUp from "../components/CustomPopUp";
+import Header from "./../components/Header";
+import { useNavigate } from "react-router-dom";
 
 function CardInfo(props) {
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  const openPopUp = () => {
+    setPopupOpen(true);
+  };
+  const closePopUp = () => {
+    setPopupOpen(false);
+  };
+  const navigate = useNavigate();
+  const handleCardRegisterClick = () => {
+    navigate("/Home/CardRegister");
+  };
+
   return (
     <div class="card-container">
       {/* 카드명, 이미지 파일은 DB에서, 혜택들은 상세페이지 URL로 크롤링해서 가져온다. */}
       <h2>신한카드 Deep Dream</h2>
       <div>1234 5678 9928 1029</div>
       <div class="card-wrapper">
-        <button>before</button>
+        <button class="changecard-button">Before</button>
 
         <img
           src="https://www.shinhancard.com/pconts/images/contents/card/plate/cdCheckBGNDC0s.png"
-          alt="Card Image"
-          class="card-image"
+          className="card-image"
         />
-        <button>After</button>
+        <button class="changecard-button">After</button>
       </div>
 
       <div>해외 이용 수수료 면제</div>
@@ -23,8 +38,19 @@ function CardInfo(props) {
       <div>해외 대중교통 1% 결제일 할인</div>
 
       <div class="button-container">
-        <button class="action-button">신규 카드 등록</button>
-        <button class="action-button">카드 삭제</button>
+        <button class="action-button" onClick={handleCardRegisterClick}>
+          신규 카드 등록
+        </button>
+        <button class="action-button" onClick={openPopUp}>
+          카드 삭제
+        </button>
+        <CardDeltePopUp
+          open={popupOpen}
+          close={closePopUp}
+          header="PopUp Heading"
+        >
+          카드를 삭제할까요?
+        </CardDeltePopUp>
       </div>
     </div>
   );
--- src/pages/CardRegister.jsx
+++ src/pages/CardRegister.jsx
@@ -2,15 +2,51 @@ import React, { useRef, useState } from "react";
 import Footer from "../components/Footer";
 import { Camera } from "react-camera-pro";
 import "../styles/cardRegistration.css";
+import AWS from "aws-sdk";
 
 function CardRegister(props) {
   //카메라용
   const cameraRef = useRef(null);
   const [image, setImage] = useState(null);
 
+  //S#
+
   const takePicture = () => {
     const photo = cameraRef.current.takePhoto();
     setImage(photo);
+    const blob = dataURLtoBlob(photo);
+    return blob;
+  };
+
+  const getFileName = () => {
+    const timestamp = Date.now();
+    return `cardimg_${timestamp}.jpg`;
+  };
+
+  const uploadToS3 = (filename, fileBlob) => {
+    //AWS S3 설정
+    AWS.config.update({
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+      region: process.env.REACT_APP_AWS_ACCESS_REGION,
+    });
+
+    const s3 = new AWS.S3();
+
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: "shinhands3rd-project2",
+        Key: "CardIMG/" + filename,
+        Body: fileBlob,
+      },
+    });
+    upload.promise().then(console.log("업로드"));
+  };
+
+  const handleTakePhoto = () => {
+    const photoBlob = takePicture();
+    const fileName = getFileName();
+    uploadToS3(fileName, photoBlob);
   };
 
   return (
@@ -25,7 +61,7 @@ function CardRegister(props) {
               aspectRatio={4 / 3}
               facingMode={"environment"}
             />
-            <button class="capture-button" onClick={takePicture}>
+            <button class="capture-button" onClick={handleTakePhoto}>
               촬영하기
             </button>
           </>
@@ -98,6 +134,21 @@ function CardRegister(props) {
       </div>
     </div>
   );
+}
+
+// 데이터 URL을 Blob으로 변환하는 유틸리티 함수
+function dataURLtoBlob(dataURL) {
+  const arr = dataURL.split(",");
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new Blob([u8arr], { type: mime });
 }
 
 export default CardRegister;
