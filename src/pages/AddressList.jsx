1293be08af30cb7e1b47bffcb85975632f4b388f
BaekInHyeok <bih63879@gmail.com>
2024-08-16 10:15:04 +0900

    start option 수정. 주소목록 불필요한 css 삭제

--- src/pages/AddressList.jsx
+++ src/pages/AddressList.jsx
@@ -5,71 +5,9 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import CardDeltePopUp from "../components/CustomPopUp";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  background-color: #f3f4f7;
-  min-height: 100vh;
-`;
-
-const Tabs = styled.div`
-  display: flex;
-  margin-bottom: 20px;
-`;
-
-const Tab = styled.div`
-  padding: 10px 20px;
-  border-radius: 20px;
-  background-color: ${(props) => (props.active ? "#6A5ACD" : "#BFE6E1")};
-  color: ${(props) => (props.active ? "#ffffff" : "#000000")};
-  margin-right: 10px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${(props) => (props.active ? "#483D8B" : "#A0D6CF")};
-  }
-`;
-
-const Card = styled.div`
-  width: 350px;
-  padding: 20px;
-  background-color: #ffffff;
-  border-radius: 20px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-`;
-
 const Title = styled.h2`
   margin-bottom: 20px;
   text-align: center;
-`;
-
-const table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  margin-bottom: 20px;
-
-  th,
-  td {
-    border: 1px solid #e3e6eb;
-    padding: 10px;
-    text-align: left;
-  }
-
-  th {
-    background-color: #e0f7f1;
-  }
-
-  td {
-    background-color: #f7fbfc;
-  }
-
-  td:last-child {
-    text-align: center;
-    cursor: pointer;
-    color: red;
-  }
 `;
 
 const AddButton = styled.button`
@@ -101,10 +39,6 @@ function AddressList(props) {
 
   const closePopUp = () => {
     setPopupOpen(false);
-  };
-
-  const handleCardRegisterClick = () => {
-    navigate("/Home/CardRegister");
   };
 
   //백엔드에서 넘겨받는 데이터를 다음과 같이 리스트로 저장
