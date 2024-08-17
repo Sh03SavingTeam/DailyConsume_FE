2eb67c7eb59ef6454e497c76ddb721b00d1b14ac
aaa67 <enen9032@naver.com>
2024-08-17 16:00:07 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* Footer 컴포넌트를 렌더링합니다. */}
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import MapPage from "./pages/MapPage";
+import ReviewRegister from "./pages/ReviewRegister";
+import CardRegister from "./pages/CardRegister";
+import Home from "./pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+  <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home/CardRegister" element={<CardRegister />}></Route>
+        <Route path="/MapPage" element={<MapPage />}></Route>
+        <Route path="/MapPage/ReviewReg" element={<ReviewRegister />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
