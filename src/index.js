bbcb42501cc77eeb9d61a2d8ecb445a5b0cd3a1e
sangho5550 <baeksh0118@gmail.com>
2024-08-22 14:09:16 +0900

    Calendar 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./App.css";  // 전역 스타일로 불러오기
+import "./App.css"; // 전역 스타일로 불러오기
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MapPage from "./pages/MapPage";
@@ -12,23 +12,23 @@ import ConsumeHistory from "./pages/ConsumeHistory";
 import Home from "./pages/Home";
 import AddressList from "./pages/AddressList";
 import AddressRegister from "./pages/AddressRegister";
-import Calendar from "./components/Calendar";
+import Calendar from "./pages/Calendar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Home/CardRegister" element={<CardRegister />}></Route>
-        <Route path="/MapPage" element={<MapPage />}></Route>
-        <Route path="/MapPage/ReviewReg" element={<ReviewRegister />}></Route>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/ConsumeHistory" element={<ConsumeHistory />}></Route>
-        <Route path="/Home" element={<Home />}></Route>
-        <Route path="/MyPage/AddrList" element={<AddressList />}></Route>
-        <Route path="/MyPage/AddrRegister" element={<AddressRegister />}></Route>
-        <Route path="/Calendar" element={<Calendar />}></Route>
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/Home/CardRegister" element={<CardRegister />}></Route>
+      <Route path="/MapPage" element={<MapPage />}></Route>
+      <Route path="/MapPage/ReviewReg" element={<ReviewRegister />}></Route>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/ConsumeHistory" element={<ConsumeHistory />}></Route>
+      <Route path="/Home" element={<Home />}></Route>
+      <Route path="/MyPage/AddrList" element={<AddressList />}></Route>
+      <Route path="/MyPage/AddrRegister" element={<AddressRegister />}></Route>
+      <Route path="/Calendar" element={<Calendar />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 reportWebVitals();
